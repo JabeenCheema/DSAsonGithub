17a42198b9f4017b741545356e2eccfbfd2b8f52
Jabeen Cheema <jabeencheema@pursuit.org>
2019-12-07 19:42:58 -0500

    adding files

---
+++ StringsDSA.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+/*
+Write a function that reverses a string. The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+ 
+ Example
+ Input: ["h","e","l","l","o"]
+ Output: ["o","l","l","e","h"]
+*/
+
+
+//func reverseString(input: inout[Character]) {
+//    var i = 0
+//    var j = input.count - 1
+//
+//    while i < j {
+//        input.swapAt(i, j)
+//        i += 1
+//        j -= 1
+//    }
+//
+//}
+
+
+
